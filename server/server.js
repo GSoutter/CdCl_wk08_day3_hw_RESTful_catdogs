fc3983323e6f30b5407350e924970b4dba81dee1
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-29 19:19:56 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+
+
+
+
+const dogsdata = [
+  { name: "Fluffy", breed: "Pomeranian" },
+  { name: "Kane", breed: "Rottweiler" },
+  { name: "Bonzo", breed: "German Shepherd" },
+  { name: "Lucky", breed: "Cockapoo" },
+  { name: "Luna", breed: "Husky" }
+];
