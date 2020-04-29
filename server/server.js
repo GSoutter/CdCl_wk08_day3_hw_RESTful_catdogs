252b94569ec471f0d6187521968ee71ff932e13b
Gordon Soutter <gordon.soutter@outlook.com>
2020-04-29 19:53:36 +0100

    adds get all, get dog id, post dog

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const createRouter = require('./createRouter.js');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
 
 
-
-
-const dogsdata = [
+const dogs = [
   { name: "Fluffy", breed: "Pomeranian" },
   { name: "Kane", breed: "Rottweiler" },
   { name: "Bonzo", breed: "German Shepherd" },
   { name: "Lucky", breed: "Cockapoo" },
   { name: "Luna", breed: "Husky" }
 ];
+
+
+const dogsRouter = createRouter(dogs);
+app.use('/api/dogs/', dogsRouter);
+
+
+app.listen(3000, function () {
+  console.log(`App running on port ${ this.address().port }`);
+});
